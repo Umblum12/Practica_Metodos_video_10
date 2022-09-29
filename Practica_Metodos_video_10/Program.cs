2525cd5cbf96f0cf3db91f46cf545d33f58f34ba
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:37:07 -0500

    Initial commit

---
+++ Practica_Metodos_video_10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practica_Metodos_video_10
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            sumaNumeros(10,9);
+            Console.ReadKey(true);
+        }
+
+
+        static void sumaNumeros(int num1, int num2)
+        {
+            Console.WriteLine($"La suma de los números es: {num1+num2}");
+        }
+    }
+}
+
